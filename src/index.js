b07dd6847e38d995aa1a1c800e467e2e7e243262
ilan7empest <therecanbeonly1.1984@gmail.com>
2022-04-19 11:06:27 +0300

    structure changes

--- src/index.js
+++ src/index.js
@@ -1,17 +1,5 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.scss'
-import App from './App'
-import reportWebVitals from './reportWebVitals'
+import * as components from './lib/components'
+import * as types from './lib/types'
+import * as constants from './lib/constants'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-)
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals()
+export { components, constants, types }
